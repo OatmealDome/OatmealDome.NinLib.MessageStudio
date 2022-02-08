71dae26a576a53c054ac99ef72bed956462e9cf8
OatmealDome <julian@oatmealdome.me>
2022-02-08 03:15:16 -0500

    Msbt: Add class to read MSBT files

---
+++ OatmealDome.NinLib.MessageStudio/Msbt.cs
@@ -0,0 +1,147 @@
+using System.Diagnostics;
+using System.Text;
+using OatmealDome.BinaryData;
+
+namespace OatmealDome.NinLib.MessageStudio;
+
+public sealed class Msbt
+{
+    private readonly Dictionary<string, string> _values = new Dictionary<string, string>();
+    private readonly Dictionary<string, int> _labelValueIdx = new Dictionary<string, int>();
+    private readonly List<string> _strings = new List<string>();
+
+    public IEnumerable<string> Keys => _values.Keys;
+
+    public Msbt(byte[] data)
+    {
+        using (MemoryStream memoryStream = new MemoryStream(data))
+        {
+            Read(memoryStream);
+        }
+    }
+
+    public Msbt(Stream stream)
+    {
+        Read(stream);
+    }
+
+    public bool ContainsKey(string label)
+    {
+        return _values.ContainsKey(label);
+    }
+
+    public string Get(string label)
+    {
+        return _values[label];
+    }
+
+    // Based on MSBT file format documentation on ZeldaMods
+    private void Read(Stream stream)
+    {
+        using BinaryDataReader reader = new BinaryDataReader(stream);
+
+        // Set endianness to big by default
+        reader.ByteOrder = ByteOrder.BigEndian;
+
+        // Verify the magic numbers
+        if (reader.ReadString(8) != "MsgStdBn")
+        {
+            throw new Exception("Not a MSBT file");
+        }
+
+        // Read BOM
+        if (reader.ReadByte() == 0xFF && reader.ReadByte() == 0xFE)
+        {
+            reader.ByteOrder = ByteOrder.LittleEndian;
+        }
+
+        reader.Seek(2); // padding?
+
+        ushort version = reader.ReadUInt16();
+        ushort sectionCount = reader.ReadUInt16();
+
+        reader.Seek(2); // padding?
+
+        uint fileSize = reader.ReadUInt32();
+
+        reader.Seek(10); // padding?
+
+        for (int i = 0; i < sectionCount; i++)
+        {
+            Trace.Assert(reader.Position % 0x10 == 0);
+
+            string sectionMagic = reader.ReadString(4);
+            uint sectionSize = reader.ReadUInt32();
+
+            reader.Seek(8); // padding
+
+            long sectionStart = reader.Position;
+
+            switch (sectionMagic)
+            {
+                case "LBL1":
+                    ReadLabelsSection(reader);
+                    break;
+                case "TXT2":
+                    ReadTextSection(reader);
+                    break;
+                default:
+                    // ATR1 not implemented
+                    break;
+            }
+
+            // Seek to next table
+            reader.Seek(sectionStart + sectionSize, SeekOrigin.Begin);
+            reader.Align(0x10);
+        }
+
+        foreach (KeyValuePair<string, int> labelPair in _labelValueIdx)
+        {
+            _values[labelPair.Key] = _strings[labelPair.Value];
+        }
+    }
+
+    private void ReadLabelsSection(BinaryDataReader reader)
+    {
+        long startOffset = reader.Position;
+
+        uint offsetCount = reader.ReadUInt32();
+
+        for (uint i = 0; i < offsetCount; i++)
+        {
+            uint stringCount = reader.ReadUInt32();
+            uint stringOffset = reader.ReadUInt32();
+
+            using (reader.TemporarySeek(startOffset + stringOffset, SeekOrigin.Begin))
+            {
+                for (uint j = 0; j < stringCount; j++)
+                {
+                    int stringLength = reader.ReadByte();
+                    string label = reader.ReadString(stringLength);
+                    int textTableIndex = (int)reader.ReadUInt32();
+
+                    _labelValueIdx[label] = textTableIndex;
+                }
+            }
+        }
+    }
+
+    private void ReadTextSection(BinaryDataReader reader)
+    {
+        long startOffset = reader.Position;
+
+        uint offsetCount = reader.ReadUInt32();
+
+        for (uint i = 0; i < offsetCount; i++)
+        {
+            uint stringOffset = reader.ReadUInt32();
+
+            using (reader.TemporarySeek(startOffset + stringOffset, SeekOrigin.Begin))
+            {
+                string label = reader.ReadString(StringDataFormat.ZeroTerminated, Encoding.Unicode);
+
+                _strings.Add(label);
+            }
+        }
+    }
+}
